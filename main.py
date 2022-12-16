ed7a1f64b00c1f03364bddf79b251a959e4c14f3
ayoub3bidi <mrayoubabidi@gmail.com>
2022-12-16 17:23:34 +0100

    🚚 passing arguments

--- main.py
+++ main.py
@@ -4,11 +4,19 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+names = {
+        'tim': {'age': 19, 'gender': 'male' },
+        'bill': {'age': 24 , 'gender': 'male' }
+        }
 
-api.add_resource(HelloWorld, '/hello')
+class HelloWorld(Resource):
+    def get(self, name):
+        return names[name]
+
+    def post(self):
+        return {'data': 'posted'}
+
+api.add_resource(HelloWorld, '/hello/<string:name>')
 
 if __name__ == "__main__":
     app.run(debug=True)
--- test-request.py
+++ test-request.py
@@ -2,5 +2,5 @@ import requests
 
 BaseURL = "http://127.0.0.1:5000"
 
-response = requests.get(BaseURL + "/hello")
+response = requests.get(BaseURL + "/hello/tim")
 print(response.json())
