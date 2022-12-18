a23c4801ac2d9ec463299297574b5aa335c1bf5f
ayoub3bidi <mrayoubabidi@gmail.com>
2022-12-18 20:31:28 +0100

    ✅ request argument parser

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
-names = {
-        'tim': {'age': 19, 'gender': 'male' },
-        'bill': {'age': 24 , 'gender': 'male' }
-        }
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
+video_put_args.add_argument("views", type=int, help="View of the video is required", required=True)
+video_put_args.add_argument("likes", type=int, help="Likes on the video is required", required=True)
 
-class HelloWorld(Resource):
-    def get(self, name):
-        return names[name]
+names = {'tim': {'age': 19, 'gender': 'male' },'bill': {'age': 24 , 'gender': 'male' }}
 
-    def post(self):
-        return {'data': 'posted'}
+# class HelloWorld(Resource):
+#     def get(self, name):
+#         return names[name]
 
-api.add_resource(HelloWorld, '/hello/<string:name>')
+#     def post(self):
+#         return {'data': 'posted'}
+
+# api.add_resource(HelloWorld, '/hello/<string:name>')
+
+videos = {}
+
+class Video(Resource):
+    def get(self, video_id):
+        return videos[video_id]
+
+    def put(self, video_id):
+        args = video_put_args.parse_args()
+        return {video_id: args}
+
+api.add_resource(Video, '/video/<int:video_id>')
 
 if __name__ == "__main__":
     app.run(debug=True)
--- test-request.py
+++ test-request.py
@@ -2,5 +2,5 @@ import requests
 
 BaseURL = "http://127.0.0.1:5000"
 
-response = requests.get(BaseURL + "/hello/tim")
+response = requests.put(BaseURL + "/video/1", {"name": "tim", "views": 100, "likes": 10})
 print(response.json())
